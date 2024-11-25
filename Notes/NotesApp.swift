4a64090167e737d707d240cd4ce7b75c9b0f1fa3
Santiago Mendoza <santiagomendozaa6@gmail.com>
2024-11-25 14:49:11 -0500

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Santiago Mendoza on 25/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
